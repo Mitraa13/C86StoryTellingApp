72861bfa167b2467e8f82754729686873fc65e8b
Mitraa <72913943+Mitraa13@users.noreply.github.com>
2021-07-09 21:14:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import DashboardScreen from './screens/DashboardScreen';
+import LoadingScreen from './screens/LoadingScreen';
+import LoginScreen from './screens/LoginScreen';
+import firebase  from "firebase";
+import { firebaseConfig } from "./config";
+
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+else{
+  firebase.app()
+}
+
+export default function App() {
+  return (
+      <AppContainer />
+  );
+}
+
+const SwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen,
+})
+
+const AppContainer = createAppContainer(SwitchNavigator);
---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/app';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDnW0ipdXqDBdcbLL8hPYI9ve57LpdBb98",
+  authDomain: "storytellingapp-9eea9.firebaseapp.com",
+  databaseURL: "https://storytellingapp-9eea9-default-rtdb.firebaseio.com",
+  projectId: "storytellingapp-9eea9",
+  storageBucket: "storytellingapp-9eea9.appspot.com",
+  messagingSenderId: "248210940024",
+  appId: "1:248210940024:web:94f094284bf925995dcd8d"
+};
